1feb981f70856e394b5f86bff1a81d0a34400815
hannahgoeritz <hannah.goeritz@frequentis.com>
2018-03-20 09:30:14 +0100

    removed currently unnecessary files

--- admin-frontend/src/router/index.js
+++ admin-frontend/src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Tools from '@/components/Tools'
 import Overview from '@/components/Overview'
-import ConfigureGateways from '@/components/ConfigureGateways'
 
 Vue.use(Router)
 
