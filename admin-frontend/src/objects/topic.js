4cc360b844f5f34c7361ffd15fb6b4d301eb4146
GOERITZ Hannah <hannah.goeritz@frequentis.com>
2018-07-19 11:54:43 +0200

    Removed some comments and console outputs.

--- admin-frontend/src/objects/topic.js
+++ admin-frontend/src/objects/topic.js
@@ -1,11 +1,11 @@
 export function Topic(topic) {
   this.id = topic.id,
     this.clientId = topic.clientId,
-    this.type = topic.type, //getAllTopicTypes
+    this.type = topic.type,
     this.name= topic.name,
     this.state = topic.state || false,
-    this.msgType = topic.msgType, //getAllStandards
-    this.msgTypeVersion = topic.msgTypeVersion, //getAllStandards
+    this.msgType = topic.msgType,
+    this.msgTypeVersion = topic.msgTypeVersion,
     this.description = topic.description,
     this.publishSolutionIDs = topic.publishSolutionIDs,
     this.showDescription = false
