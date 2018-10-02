c7dcedbf947f261603a774f53134e033050cc40e
hannahgoeritz <hannah.goeritz@frequentis.com>
2018-10-02 15:38:34 +0200

    Trial can be startet more than once

--- admin-frontend/src/App.vue
+++ admin-frontend/src/App.vue
@@ -5,7 +5,7 @@
       <v-toolbar-title class="title">Admin tool</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn :disabled="isTestbedInitialized" v-on:click="initTestbed">Initialize testbed</v-btn>
-      <v-btn :disabled="!isTestbedInitialized || isTrialStarted" v-on:click="startTrial">Start trial</v-btn>
+      <v-btn :disabled="!isTestbedInitialized" v-on:click="startTrial">Start trial</v-btn>
     </v-toolbar>
     <main>
       <router-view></router-view>
